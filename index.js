b722f55e575fcb7c58769cefe10c64b4d3852364
pedroblima <pedrob@iesb.edu.br>
2023-11-03 20:08:09 -0300

    adicionado mais lanche

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const server =  express();
 const delivery = require('./src/data/delivery.json');
+const lanche = require('./src/data/lanche.json')
 
 server.get('/delivery', (req, res) => {
-    return res.json({delivery});
+    return res.json({delivery, lanche});
 })
 
 server.listen(3000, () => {
