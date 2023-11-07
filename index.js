417afa7b9626679c42e4336dfef27416046a7ee3
pedroblima <pedrob@iesb.edu.br>
2023-11-07 20:50:48 -0300

    backend

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const server =  express();
 const delivery = require('./src/data/delivery.json');
+const lanches = require('./src/data/lanche.json');
 
 server.get('/delivery', (req, res) => {
-    return res.json({delivery});
+    return res.json(delivery);
+})
+server.get('/lanche', (req, res) => {
+    return res.json(lanches);
 })
 
 server.listen(3000, () => {
